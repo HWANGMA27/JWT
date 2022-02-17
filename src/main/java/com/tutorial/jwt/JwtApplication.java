4bcaf822eaa20c07b947e969bbe176b753170b39
hwangma27 <pikacu22@kakao.com>
2022-02-17 22:36:11 +0900

    [ADD] JWT Config 추가

---
+++ src/main/java/com/tutorial/jwt/JwtApplication.java
@@ -0,0 +1,19 @@
+package com.tutorial.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class JwtApplication {
+
+    private static final String APPLICATION =
+            "spring.config.location=" +
+                    "classpath:/application.yml," +
+                    "classpath:/application_auth.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(JwtApplication.class).properties(APPLICATION).run(args);
+    }
+
+}
---
+++ src/main/java/com/tutorial/jwt/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,19 @@
+package com.tutorial.jwt.jwt;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+   @Override
+   public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+      //권한 부족 에러 403
+      response.sendError(HttpServletResponse.SC_FORBIDDEN);
+   }
+}
